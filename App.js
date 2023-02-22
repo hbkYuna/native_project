5b58e0f68e6280326643af7a2e5c2a157e872d49
hbkYuna <strawhatnxahx@gmail.com>
2023-02-22 12:25:41 +0100

    Navigation (stack) added and working + SafeAreaView

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from "./Screens/HomeScreen";
+import FavouritePlaces from "./Screens/FavouritePlaces";
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Welcome" component={HomeScreen} />
+          <Stack.Screen name="FavouritePlaces" component={FavouritePlaces} />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ Screens/FavouritePlaces.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
+
+function Banner() {
+    return null;
+}
+
+export function FavouritePlaces() {
+
+    return (
+        <View>
+            <Banner/>
+            <Text>My Favourite Places</Text>
+        </View>
+    )
+
+}
+
+export default FavouritePlaces
+
